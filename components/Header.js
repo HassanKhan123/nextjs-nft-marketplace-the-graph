9274986ec84b4875b4abb911626261caa9734c19
HassanKhan123 <hk2602809@gmail.com>
2022-07-26 18:36:06 +0500

    added: tailwindcss setup

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,22 @@
-import React from "react";
 import { ConnectWallet } from "@web3uikit/web3";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
-      <ConnectWallet />
-    </div>
+    <>
+      <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+        <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+        <div className="flex flex-row items-center">
+          <Link href="/">
+            <a className="mr-4 p-6">Home</a>
+          </Link>
+          <Link href="/sell-nft">
+            <a className="mr-4 p-6">Sell NFT</a>
+          </Link>
+          <ConnectWallet moralisAuth={false} />
+        </div>
+      </nav>
+    </>
   );
 };
 
