1a2d5915e43631fc9261bdd434c34a0b899b040f
HassanKhan123 <hk2602809@gmail.com>
2022-07-26 15:30:52 +0500

    initial project setup

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ConnectWallet } from "@web3uikit/web3";
+
+const Header = () => {
+  return (
+    <div>
+      <ConnectWallet />
+    </div>
+  );
+};
+
+export default Header;
